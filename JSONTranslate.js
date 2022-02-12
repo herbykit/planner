eff60a17a3bef894649130e02883ce7e879a152c
herbykit <litichiyt@gmail.com>
2022-02-12 09:36:25 -0500

    Fixing error with pull request

--- JSONTranslate.js
+++ JSONTranslate.js
@@ -3,21 +3,25 @@ const fs = require("fs");
 
 // Recipes
 // Translates data into JSON formatted and inserts it into the user's JSON storage
-function getItemSyntax( item, number, size ) {
+function getItemSyntax(item, number, size) {
 	// Creates a JSON string to combine with others to create the array
 	// needed in the translateRecipe and translateGrocery function
-	var item_added = {}; // Input salt from tears of no simpler way to create it...
-	item_added[item] = { 'amount': 0, 'size': "null" }; // Creates the structure to fill in
+	let item_added = {}; // Input salt from tears of no simpler way to create it...
+	item_added[item] = { amount: 0, size: "null" }; // Creates the structure to fill in
 	item_added[item]["amount"] = number; // Resets the number to the correct amount
 	item_added[item]["size"] = size; // Resets the size to the correct amount
 	return item_added;
 }
 
-function combineItems( ...items ) {
+function combineItems(items) {
 	// combines an unknown size of items to one JSON object
+	// Uses empty argument to allow infinite arguments accessed
+	// through the arguments object
+	if (!items) return {};
+	return items;
 }
 
-function translateRecipe( ingredients, instructions ) {
+function translateRecipe(ingredients, instructions) {
 	// Ingredients is an array that matches the structure of the ingredients below already
 	// Instructions is a string that includes \n values as neededd
 	// Inserts the object into the right spot
@@ -62,7 +66,7 @@ function translateRecipe( ingredients, instructions ) {
 // Groceries
 // Translates data into JSON formatted and inserts it into the user's JSON storage
 
-function translateGrocery( groceries ) {
+function translateGrocery(groceries) {
 	// Only here for structural context, most of its functions are performed already
 	// by the time it gets to here. Inserts the values into the right place.
 }
@@ -119,4 +123,9 @@ function translateGrocery( groceries ) {
 	}]
 */
 
-module.exports = { translateRecipe, translateGrocery, getItemSyntax, combineItems }
+module.exports = {
+	translateRecipe,
+	translateGrocery,
+	getItemSyntax,
+	combineItems,
+};
