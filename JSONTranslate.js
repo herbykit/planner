34ebcab644651bd99a25a381ed732209e7b34968
herbykit <litichiyt@gmail.com>
2022-02-11 20:35:52 -0500

    Pushing progress on combineItems
    Created and began testing of combining items into one object literal. Impending rewrite to stop having the extra undefined objects added at the end of the overarching object.

--- JSONTranslate.js
+++ JSONTranslate.js
@@ -13,8 +13,18 @@ function getItemSyntax( item, number, size ) {
 	return item_added;
 }
 
-function combineItems( ...items ) {
+function combineItems() {
 	// combines an unknown size of items to one JSON object
+	// Uses empty argument to allow infinite arguments accessed
+	// through the arguments object
+	var fullObject = { 0: {} };
+
+	const keys = Object.keys(arguments);
+	for (let i = 0; i < keys.length; i++) {
+		fullObject[0][keys[i]] = arguments[i][keys[i]];
+	}
+
+	return fullObject;
 }
 
 function translateRecipe( ingredients, instructions ) {
