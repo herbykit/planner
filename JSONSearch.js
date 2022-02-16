c8f7f35ce8441b9c04cfd19c3997e09d1072af97
herbykit <litichiyt@gmail.com>
2022-02-16 15:25:19 -0500

    Added general functions for handling adding recipes
    also to avoid inserting duplicate items

--- JSONSearch.js
+++ JSONSearch.js
@@ -1,3 +1,20 @@
 // Code to search for repeated recipe names
 
+function searchRecipes(name, ID) {
+  // Loop through the keys in the recipes to see if any of the names match
+}
+
 // Code to search for repeated grocery names
+function searchGroceries(name, ID) {
+  // Loop throuhg the keys in the groceries to see if any of the names match
+}
+
+function pushRecipes(ID, recipeName, ingredientList, instructions = "") {
+  // Search recipes, then push through to add the recipe to the list
+  // if it doesnt exist
+}
+
+function pushGrocery(ID, name, amount, size) {
+  // Search groceries, then push through to add the recipe to the list
+  // if it doesnt exist
+}
