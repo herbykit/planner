e5884d6cc3e6507fc5d402296b04a4874e438aff
herbykit <litichiyt@gmail.com>
2022-02-27 14:30:01 -0500

    Fixed a bug in the testing error; needed additionally layer of functions

--- test/JSONSearch.test.js
+++ test/JSONSearch.test.js
@@ -1,5 +1,6 @@
 let JSONTranslate = require("../JSONTranslate");
 let JSONSearch = require("../JSONSearch");
+const fs = require("fs");
 
 let item2 = JSONTranslate.getItemSyntax("potato", 1, "potato");
 let item3 = JSONTranslate.getItemSyntax("butter", 2, "tbsp");
@@ -18,11 +19,14 @@ test("tests that groceries matches combineItems", () => {
   ).toStrictEqual([{ garlic_powder: { amount: 4, size: "tsp" } }]);
 });
 
-test("tests that groceries matches combineItems", () => {
-  expect(JSONSearch.pushGrocery("123", "garlic_powder", 3, "tbsp")).toThrow(
-    "Incorrect unit"
-  );
+test("test that Incorrect Unit is thrown", () => {
+  expect(() => {
+    JSONSearch.pushGrocery("123", "garlic_powder", 3, "tbsp");
+  }).toThrow("Incorrect unit");
 });
+if (fs.existsSync("./data/123.json")) {
+  fs.unlinkSync("./data/123.json");
+}
 
 // pushRecipes(ID, recipeName, ingredientList, (instructions = ""));
 let recipeList = JSONSearch.pushRecipe(
@@ -60,3 +64,6 @@ test("tests that a multiple item list is created when inputting an array of reci
     },
   ]);
 });
+if (fs.existsSync("./data/123.json")) {
+  fs.unlinkSync("./data/123.json");
+}
